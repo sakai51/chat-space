03c4ddd003b126762aa701e03b75fc9f8e511687
“sakai51” <stakashi51@gmail.com>
2019-10-15 20:57:33 +0900

    commit

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,63 @@
+$(document).on('turbolinks:load', function() {
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${content}
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+  return html;
+  }
+
+  // 送信後スクロールする関数
+  function scrollBottom(){
+    var target = $('.message').last();
+    var position = target.offset().top + $('.messages').scrollTop();
+    $('.messages').animate({
+      scrollTop: position
+    }, 300, 'swing');
+  }
+
+  $('.new_message').on('submit', function(e) {
+    e.preventDefault();
+    // console.log(this)
+    var message = new FormData(this); //フォームに入力した値を取得
+    var url = $(this).attr('action'); // var url = (window.location.href)でもOK
+    $.ajax({  
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val(''); //文字だけリセット→これはうまくいくけど、文字だけのリセット
+      $('form')[0].reset();
+      // これを記述するとプログラムが動かない。送信が１回しかできなくなり、リセットもされない
+      scrollBottom(); //送信後スクロースする関数呼び出し
+
+    })
+
+    .fail(function(data) {
+      alert('error');
+    })
+    .always(function(data) {
+      $('.form__submit').prop('disabled', false); //ここで解除している
+    })
+  })
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,10 @@
+json.id      @message.id
+json.content @message.content 
+json.date    @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name @message.user.name
+json.image   @message.image.url
+
+
+# json.text  @message.text
+# json.user_id  @message.user.id
+# json.user_name  @message.user.name
