0b10c202d294c1980032ee54eb08f92a02ef9b39
“sakai51” <stakashi51@gmail.com>
2019-10-16 20:01:06 +0900

    commit 2000

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%") #「?」はプレースホルダと言うもので、第2引数の値を「?」へ置き換えるための目印です。
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
