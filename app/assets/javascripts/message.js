7dc3e7686d9abee768f65ef4bbaa355289fbe580
“sakai51” <stakashi51@gmail.com>
2019-10-17 19:55:40 +0900

    commit 1955

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,112 +1,25 @@
 $(document).on('turbolinks:load', function() {
-  var buildHTML = function(message) {
+  function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
-    var img = message.image ? `<img src= ${ message.image } class="lower-message__image">` : "";
-    if (message.content && message.image) {
-      //data-idが反映されるようにしている
-
-      var html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.date}
-                      </div>
+    var img = message.image ? `<img src="${ message.image }">` : "";
+    var html = `<div class="message" data-message-id="${ message.id }">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
                     </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${content}
-                      </p>
-                        ${img}
+                    <div class="upper-message__date">
+                      ${message.date}
                     </div>
-                  </div>`
-      
-      // var html = '<div class="message" data-message-id=' + message.id + '>' +
-      //   '<div class="upper-message">' +
-      //     '<div class="upper-message__user-name">' +
-      //       message.user_name +
-      //     '</div>' +
-      //     '<div class="upper-message__date">' +
-      //       message.date +
-      //     '</div>' +
-      //   '</div>' +
-      //   '<div class="lower-message">' +
-      //     '<p class="lower-message__content">' +
-      //       content +
-      //     '</p>' +
-      //     img +
-      //   '</div>' +
-      // '</div>'
-
-    } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-
-      var html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.date}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${content}
-                      </p>
-                    </div>
-                  </div>`
-
-      // var html = '<div class="message" data-message-id=' + message.id + '>' +
-      //   '<div class="upper-message">' +
-      //     '<div class="upper-message__user-name">' +
-      //       message.user_name +
-      //     '</div>' +
-      //     '<div class="upper-message__date">' +
-      //       message.date +
-      //     '</div>' +
-      //   '</div>' +
-      //   '<div class="lower-message">' +
-      //     '<p class="lower-message__content">' +
-      //       content +
-      //     '</p>' +
-      //   '</div>' +
-      // '</div>'
-
-    } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
-
-      var html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.date}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      ${img}
-                    </div>
-                  </div>`
-
-      // var html = '<div class="message" data-message-id=' + message.id + '>' +
-      //   '<div class="upper-message">' +
-      //     '<div class="upper-message__user-name">' +
-      //       message.user_name +
-      //     '</div>' +
-      //     '<div class="upper-message__date">' +
-      //       message.date +
-      //     '</div>' +
-      //   '</div>' +
-      //   '<div class="lower-message">' +
-      //     img +
-      //   '</div>' +
-      // '</div>'
-    };
-    return html;
-  };
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${content}
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+  return html;
+  }
 
   // 送信後スクロールする関数
   function scrollBottom(){
@@ -120,8 +33,8 @@ $(document).on('turbolinks:load', function() {
   // インクリメンタルサーチの実装
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
-    var message = new FormData(this); //フォームに入力した値を取得
-    var url = $(this).attr('action'); // var url = (window.location.href)でもOK
+    var message = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({  
       url: url,
       type: 'POST',
@@ -148,27 +61,28 @@ $(document).on('turbolinks:load', function() {
 
   //自動更新
   var reloadMessages = function () {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
-      var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id");
 
-      $.ajax({ //ajax通信で以下のことを行う
-        url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
-        type: 'get', //メソッドを指定
-        dataType: 'json', //データはjson形式
-        data: {last_id: last_message_id} //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {last_id: last_message_id}
       })
-      .done(function (messages) { //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
-        var insertHTML = '';//追加するHTMLの入れ物を作る
-        messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-          insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
-          $('.messages').append(insertHTML);//メッセージを追加
+      .done(function (messages) {
+        console.log('ok')
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
         })
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       })
       .fail(function () {
-        alert('自動更新に失敗しました');//ダメだったらアラートを出す
+        alert('自動更新に失敗しました');
       });
     }
   };
-  setInterval(reloadMessages, 5000);//5000ミリ秒ごとにreloadMessagesという関数を実行し自動更新を行う。
+  setInterval(reloadMessages, 5000);
 });
