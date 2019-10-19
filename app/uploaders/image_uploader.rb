37dc50d923e43870aa9c338f278fef816668b64e
“sakai51” <stakashi51@gmail.com>
2019-10-19 11:47:20 +0900

    coomit 1147

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 # ↑これのコメントアウトを外して、MiniMagick経由で画像のリサイズを行えるようにしました。
   process resize_to_fit: [800, 800]
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
   storage :fog
 
   # Override the directory where uploaded files will be stored.
