db5c84caa3be5bf98b9e3ae181d72187fcad0a5b
“sakai51” <stakashi51@gmail.com>
2019-10-19 16:05:40 +0900

    commit 1610

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -38,7 +38,7 @@ $(document).on('turbolinks:load', function(){
       })
       .done(function(users){
         $("#user-search-result").empty();
-        if (users.length !== 0) {
+        if (users.length !== 0 && input.length !== 0) {
           users.forEach(function(user){
             appendUser(user);
           });
--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -78,6 +78,9 @@ $(document).on('turbolinks:load', function() {
           $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
         })
       })
+      // .fail(function () {
+      //   alert('自動更新に失敗しました');
+      // });
     }
   };
   setInterval(reloadMessages, 5000);
