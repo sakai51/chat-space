55e9a8c825032ee1f9aae329499da6d504a82750
“sakai51” <stakashi51@gmail.com>
2019-10-19 18:06:58 +0900

    commit 1805

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -28,30 +28,39 @@ $(document).on('turbolinks:load', function(){
     member_list.append(html);
       }
   
-    $("#user-search-field").on("keyup", function() {
-      var input = $("#user-search-field").val();
-      $.ajax({
-        type: 'GET',
-        url: '/users',
-        data: { keyword: input },
-        dataType: 'json'
-      })
-      .done(function(users){
-        $("#user-search-result").empty();
-        if (users.length !== 0 && input.length !== 0) {
-          users.forEach(function(user){
-            appendUser(user);
-          });
-        }
-        else {
-          appendNoUser("一致するユーザはいません");
-        }
-      })
-      .fail(function(){
-        alert('検索に失敗しました');
-      })
-    });
+      $('#user-search-field').on('keyup', function(e){
+        var input = $("#user-search-field").val();
 
+        $.ajax({
+            type: 'GET',                // HTTPメソッドはGETで
+            url:  '/users',             // /usersのURLに (これによりusersコントローラのindexアクションが起動)
+            data: { keyword: input},    // keyword: inputを送信する
+            dataType: 'json'            // サーバから値を返す際はjsonである
+        })
+
+        .done(function(users){                // usersにjson形式のuser変数が代入される。複数形なので配列型で入ってくる
+
+            if (input.length === 0) {         // フォームの文字列長さが0であれば、インクリメンタルサーチ結果を表示しないようにする
+                $('#user-search-result').empty();
+              }
+
+            else if (input.length !== 0){     // 値が等しくないもしくは型が等しくなければtrueを返す。
+                $('#user-search-result').empty();
+                users.forEach(function(user){ // users情報をひとつずつとりだしてuserに代入
+                    appendUser(user)
+                });
+            }
+
+            else {
+                $('#user-search-result').empty(); // ユーザーが見つからなければ「見つからない」を返す。
+                appendErrMsgToHTML("一致するユーザーが見つかりません");
+            }
+        })
+
+        .fail(function() {
+            alert('ユーザー検索に失敗しました');
+        });
+    });
       $(document).on("click", ".user-search-add", function () {
       $('#chat-group-users').val();
         var userId = $(this).data('user-id');
